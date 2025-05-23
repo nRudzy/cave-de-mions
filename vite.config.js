fe79bf97a5b276c30e16a7b994a4217bd78eacd9
gdealmeida <gdealmeida@stsweb.fr>
2025-05-23 17:51:48 +0200

    prepare deploy prod

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: './',
   server: {
     host: true,
     port: 3000,
@@ -16,4 +17,4 @@ export default defineConfig({
       '@': '/src'
     }
   }
-}) 
+})
